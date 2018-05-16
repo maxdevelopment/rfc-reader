39368bf537a566f58b40cd0e5d83d716003a7dde
Maksim Naranovich <maksim.naranovich@oxagile.com>
2018-05-16 16:07:43 +0300

    const

--- main.go
+++ main.go
@@ -1,4 +1,17 @@
 package main
 
+func init() {
+	const (
+		defaultRrcQuantity   = 1000
+		defaultRfcQuantityHelper   = "defaultRfcQuantityHelper"
+		popularWordsQuantity = 20
+		popularWordsQuantityHelper = "popularWordsQuantityHelper"
+		popularWordLength    = 4
+		popularWordLengthHelper    = "popularWordLengthHelper"
+		connectionsQuantity  = 10
+		connectionsQuantityHelper  = "connectionsQuantityHelper"
+	)
+}
+
 func main() {
 }
