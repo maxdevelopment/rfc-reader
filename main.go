679a52f568d86778efab86bedb69811e0f5b92c1
MaxN <gtamadmax@gmail.com>
2018-05-17 00:52:03 +0300

    app console flags

--- main.go
+++ main.go
@@ -1,17 +1,30 @@
 package main
 
-func init() {
-	const (
-		defaultRrcQuantity   = 1000
-		defaultRfcQuantityHelper   = "defaultRfcQuantityHelper"
-		popularWordsQuantity = 20
-		popularWordsQuantityHelper = "popularWordsQuantityHelper"
-		popularWordLength    = 4
-		popularWordLengthHelper    = "popularWordLengthHelper"
-		connectionsQuantity  = 10
-		connectionsQuantityHelper  = "connectionsQuantityHelper"
-	)
-}
+import (
+	"flag"
+	"fmt"
+)
+
+const (
+	defaultRrcQuantity         = 1000
+	defaultRfcQuantityHelper   = "quantity RFC for wrapping"
+	popularWordsQuantity       = 20
+	popularWordsQuantityHelper = "popular words quantity"
+	popularWordLength          = 4
+	popularWordLengthHelper    = "popular words minimal length"
+	connectionsQuantity        = 10
+	connectionsQuantityHelper  = "connections quantity"
+)
 
 func main() {
+	rpcQty := flag.Int("rpcQty", defaultRrcQuantity, defaultRfcQuantityHelper)
+	popWordsQty := flag.Int("wordsQty", popularWordsQuantity, popularWordsQuantityHelper)
+	wordLengh := flag.Int("wordLen", popularWordLength, popularWordLengthHelper)
+	connQty := flag.Int("connQty", connectionsQuantity, connectionsQuantityHelper)
+
+	flag.Parse()
+	fmt.Println(*rpcQty)
+	fmt.Println(*popWordsQty)
+	fmt.Println(*wordLengh)
+	fmt.Println(*connQty)
 }
